f92536d457c88322630e5cb26b65c4364de56380
tariqnaeem <tariq0537@gmail.com>
2018-06-06 21:19:21 +1000

    changes

--- ui/src/menu/index.js
+++ ui/src/menu/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Button from '@material-ui/core/Button';
@@ -9,8 +9,6 @@ import KeyboardVoice from '@material-ui/icons/KeyboardVoice';
 import Icon from '@material-ui/core/Icon';
 import Save from '@material-ui/icons/Save';
 import Views from '../views';
-
-var view = "INGEST";
 
 const styles = theme => ({
   button: {
@@ -27,40 +25,43 @@ const styles = theme => ({
   },
 });
 
-function loadView(type) {
+class Menu extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      view: 'INGEST',
+    };
   
-  view = 'INGEST';
-  console.log(type);
-
 }
-
-function IconLabelButtons(props) {
-  const { classes } = props;
+render() {
+  const { classes } = this.props;
   return (
     <div className="align-center">
       <div>
-        <Button variant="contained" color="primary" className={classes.button} onClick={loadView('INGEST')}>
+        <Button variant="contained" color="primary" className={classes.button} onClick={() => { this.setState({view : 'INGEST'}) }}>
           Ingest New Project
           <FileUpload className={classes.rightIcon} />
         </Button>
-        <Button variant="contained" color="default" className={classes.button} onClick={loadView('UNPUBLISH')}>
+        <Button variant="contained" color="default" className={classes.button} onClick={() => { this.setState({view : 'UNPUBLISH'}) }}>
           Manage Unpublished Project
           <Icon className={classes.rightIcon}>send</Icon>
         </Button>
-        <Button variant="contained" size="small" className={classes.button} onClick={loadView('PUBLISH')}>
+        <Button variant="contained" size="small" className={classes.button} onClick={() => { this.setState({view : 'PUBLISH'}) }}>
           <Save className={classNames(classes.leftIcon, classes.iconSmall)} />
           Manage Published Project
         </Button>
       </div>
       <div>
-        <Views view={view} />
+        <Views view={this.state.view} />
       </div>
     </div>
   );
 }
 
-IconLabelButtons.propTypes = {
+}
+
+Menu.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(IconLabelButtons);
+export default withStyles(styles)(Menu);
