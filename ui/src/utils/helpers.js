ab3cf47c00b5386aa2d00a1075be9174ea635057
Tariq Naeem <tn0i@aaa.depi.vic.gov.au>
2018-06-29 08:53:53 +1000

    Login functionality

--- ui/src/utils/helpers.js
+++
@@ -1,6 +0,0 @@
-// This function takes an array of objects and returns a new array of objects based on a unique param
-const uniq = (array, param) => {
-    return array.filter((item, pos, array) => array.map(mapItem => mapItem[param]).indexOf(item[param]) === pos);
-}
-
-export { uniq };
